ae6813735d346abb85f56eceb5a7cd4c280c2575
Юлия <yuliyamikhaylovskaya1989@gmail.com>
2025-08-27 17:41:02 +0300

    добавлен новый виджет нотбук для отображения картинок в одном окне и в разных вкладках

--- Dogs.py
+++ Dogs.py
@@ -28,12 +28,10 @@ def show_image():
             img_size = (int(width_spinbox.get()), int(height_spinbox.get()))
             img.thumbnail(img_size)
             img = ImageTk.PhotoImage(img)
-            new_window = Toplevel(window)
-            new_window.title("Случайное изображение")
-            lb = ttk.Label(new_window, image=img)
-            lb.pack()
-
-
+            tab =ttk.Frame(notebook)
+            notebook.add(tab, text=f"Картинка №{notebook.index('end')+1}")
+            lb = ttk.Label(tab, image=img)
+            lb.pack(padx=10, pady=10)
             lb.image = img
 
         except requests.RequestException as e:
@@ -72,6 +70,15 @@ height_label = ttk.Label(text="Высота:")
 height_label.pack(side='left', padx=(10, 0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 height_spinbox.pack(side='left', padx=(0, 10))
+
+top_level_window=Toplevel(window)
+top_level_window.title("Изображение собачек")
+
+notebook=ttk.Notebook(top_level_window)
+notebook.pack(expand=True,fill='both',padx=10, pady=10 )
+
+
+
 window.mainloop()
 
 
